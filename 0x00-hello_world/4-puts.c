36d02d668d26d1ef4278e6bb3f245d35a51a6f1f
zuz12345 <126672642+zuz12345@users.noreply.github.com>
2023-03-20 15:15:19 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point 
- * description: A C program that prints with put function 
+ * main - entry point
+ * description: A C program that prints with put function
  * return: always 0 (success)
  */
 int main(void)
