79c6e74a7469bf2f06a9d064afb049ca6a3ffb67
zuz12345 <126672642+zuz12345@users.noreply.github.com>
2023-03-23 16:31:39 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * main - check the code.
+ * print_alphabet_x10 - function taht will print the alphabeet 10 times 
  *
- * Return: Always 0.
- */
+*/
+
 	void print_alphabet_x10(void)
 	{
 		char ch;
