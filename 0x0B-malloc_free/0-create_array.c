5414a8260e8eb0598ae837237355e41f7b3e4244
zuz12345 <126672642+zuz12345@users.noreply.github.com>
2023-05-02 22:36:46 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,14 +8,14 @@
  * Return: pointer to array, NULL if fail
  *
  */
-	
+
 char *create_array(unsigned int size, char c)
 {
 	
 	char *str;
 	unsigned int i;
-	str = malloc(sizeof(char) * size);
 	
+	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
 		return (NULL);
 	
