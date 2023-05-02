753e513c48894df29cf856a884c2aea8b747cc3e
zuz12345 <126672642+zuz12345@users.noreply.github.com>
2023-05-02 23:00:08 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 /**
  * argstostr - main entry
- * @ac: int input	
+ * @ac: int input
  * @av: double pointer array
  * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;	
+	int i, n, r = 0, l = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
